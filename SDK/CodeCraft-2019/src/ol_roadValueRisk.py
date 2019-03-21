b90927e24cbe2f65b1770802b8dc63453dece80f
Ruige_Lee <295054118@qq.com>
2019-03-21 16:19:09 +0800

    建立道路价值体系

---
+++ SDK/CodeCraft-2019/src/ol_roadValueRisk.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @File Name: ol_roadValueRisk.py
+# @File Path: M:\MAS2\dark_PRJ\HWCC2019\SDK\CodeCraft-2019\src\ol_roadValueRisk.py
+# @Author: Ruige_Lee
+# @Date:   2019-03-21 16:15:16
+# @Last Modified by:   Ruige_Lee
+# @Last Modified time: 2019-03-21 16:18:54
+# @Email: 295054118@whut.edu.cn"
+
+
+
+
+import sys
+
+# [roadID,roadLength,maxSpeed,chnNum,startID,endID,doubleBool]
+
+
+class roadValue():
+
+	def __init__(self):
+
+
+		pass
+
+
+	def 
+
+
+
