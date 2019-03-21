60b1baefe4c7f4124684dcedb2e36e5c208fccc5
Ruige_Lee <295054118@qq.com>
2019-03-21 20:58:55 +0800

    准备测试

--- SDK/CodeCraft-2019/src/CodeCraft-2019.py
+++ SDK/CodeCraft-2019/src/CodeCraft-2019.py
@@ -4,7 +4,7 @@
 # @Author: Ruige_Lee
 # @Date:   2019-03-19 11:00:06
 # @Last Modified by:   Ruige_Lee
-# @Last Modified time: 2019-03-21 16:59:52
+# @Last Modified time: 2019-03-21 20:58:40
 # @Email: 295054118@whut.edu.cn"
 
 
@@ -13,7 +13,7 @@ import sys
 import json
 import ol_fileSystem 
 import ol_crossNetWorkOnline
-
+import ol_cl_scheduler
 # car_path = '../config/car.txt'
 # road_path = '../config/road.txt'
 # cross_path = '../config/cross.txt'
@@ -21,6 +21,8 @@ import ol_crossNetWorkOnline
 
 fS = ol_fileSystem.fS() 
 CNW = ol_crossNetWorkOnline.crossNetwork()
+sch = scheduler()
+
 
 logging.basicConfig(level=logging.DEBUG,
                     filename='../logs/CodeCraft-2019.log',
@@ -33,16 +35,6 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 
-
-def answer_init():
-	
-	fS.finalAnswer = fS.carData
-
-	# 出发时间排序
-	fS.finalAnswer.sort(key=lambda x:x[4])
-	fS.finalAnswer.sort(key=lambda x:x[3],reverse=True)
-
-	# print (finalAnswer)
 
 
 
@@ -82,37 +74,22 @@ def createAnswer():
 	# [crossID,roadID1,roadID2,roadID3,roadID4]
 
 
-	answer = fS.finalAnswer.copy()
-	fS.finalAnswer = []
+	preAnswer = []
 
-	additionalTime = 0
-	for data in answer:
-		if ( data[4] < additionalTime//25 ):
-			oneCar = [data[0],additionalTime//25]
-		else:
-			oneCar = [data[0],data[4]]
-		# print ( "Sort from ",data[1],"to",data[2] )
-
+	for data in sch.targetList:
+		
+		oneCar = [data[0],data[4]]
 
 		crossLine = CNW.createNetwork(fS.carData,fS.roadData,fS.crossData,data[1],data[2])
-
 		roadLine = cross2road(crossLine)
 
 		oneCar.extend(roadLine)
 		
 		# print ( "oneCar=",oneCar )
 
-		fS.finalAnswer.append(oneCar)
-		
-		additionalTime = additionalTime + 1 
-		# 	
-		# 	
-		# while(1):
-		# 	pass
+		preAnswer.append(oneCar)
 
-
-	pass
-
+	fS.finalAnswer = sch.fin_scheduler(preAnswer)
 
 
 def main():
@@ -139,7 +116,8 @@ def main():
 	fS.load_data(road_path,cross_path,car_path)
 
 
-	answer_init()
+	sch.pre_scheduler(fS.carData,fS.roadData,fS.crossData)
+
 	createAnswer()
 
 
---
+++ SDK/CodeCraft-2019/src/ol_scheduler.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# @File Name: ol_scheduler.py
+# @File Path: M:\MAS2\dark_PRJ\HWCC2019\SDK\CodeCraft-2019\src\ol_scheduler.py
+# @Author: Ruige_Lee
+# @Date:   2019-03-21 20:25:35
+# @Last Modified by:   Ruige_Lee
+# @Last Modified time: 2019-03-21 20:58:36
+# @Email: 295054118@whut.edu.cn"
+
+# [carID,startPos,endPos,maxSpeed,takeoffTime]
+
+import sys
+
+
+# 调度条件 本身起飞时间 、最高速度-》（规划）-》路程
+
+# 调度函数：关键构造非线性函数来压缩时间，当前主要矛盾集中在起飞时间上，而不是运行时间
+
+class scheduler():
+	def __init__(self):
+		self.carData = []
+		self.roadData = []
+		self.crossData = []
+		self.targetList = []
+		self.preAnswer = []
+
+
+
+	def pre_scheduler(self,carData,roadData,crossData):
+		self.carData = carData
+		self.roadData = roadData
+		self.crossData = crossData
+
+
+		self.targetList = self.carData.copy()
+		# 出发时间排序
+		self.targetList.sort(key=lambda x:x[4]) 
+		# 高速先排路线
+		self.targetList.sort(key=lambda x:x[3],reverse=True)
+	
+	
+	def fin_scheduler(self,preAnswer):
+
+
+		self.preAnswer = preAnswer
+
+		print("Bself.preAnswer=",self.preAnswer)
+		self.preAnswer.sort(key = lambda i:len(i)) 
+		print("Aself.preAnswer=",self.preAnswer)
+		# preAnserExtend = []
+		# print ("preAnserExtend=",preAnserExtend)
+
+		# for ans in self.preAnswer:
+		# 	ans.append(len(ans))
+		# 	preAnserExtend.append(ans)
+
+		# print( "preAnserExtend=",preAnserExtend )
+		# preAnserExtend.sort(key=lambda x:x[4])
+
+
+		# additionalTime = 0
+		
+		# 	if ( data[4] < additionalTime//25 ):
+		# 		oneCar = [data[0],additionalTime//25]
+		# 	else:
+				
+			
+		# 	additionalTime = additionalTime + 1 
+		# 	# 	
+		# 	# 	
+		# 	# while(1):
+		# 	# 	pass
+
+		return finAnswer
+
+	
+	
