a5832e06d6b744556a65ef354ac48e1f0e451aef
Ruige_Lee <295054118@qq.com>
2019-03-21 21:11:50 +0800

    顺手合并API

--- SDK/CodeCraft-2019/src/CodeCraft-2019.py
+++ SDK/CodeCraft-2019/src/CodeCraft-2019.py
@@ -4,7 +4,7 @@
 # @Author: Ruige_Lee
 # @Date:   2019-03-19 11:00:06
 # @Last Modified by:   Ruige_Lee
-# @Last Modified time: 2019-03-21 20:59:35
+# @Last Modified time: 2019-03-21 21:11:39
 # @Email: 295054118@whut.edu.cn"
 
 
@@ -36,37 +36,6 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 
-
-
-def cross2road(crossLine):
-
-	# print("crossLine=",crossLine)
-	roadLine = []
-	index = len(crossLine)
-	for i in range (0,index-1):
-		# print ( crossLine[i],crossLine[i+1] )
-		startCross = fS.crossData[crossLine[i]-1]
-		endCross = fS.crossData[crossLine[i+1]-1]
-
-		if ( (startCross[1] == endCross[1] or startCross[1] == endCross[2] or startCross[1] == endCross[3] or startCross[1] == endCross[4]) and (startCross[1] != -1) ):
-			roadLine.append( startCross[1] )
-		elif ( (startCross[2] == endCross[1] or startCross[2] == endCross[2] or startCross[2] == endCross[3] or startCross[2] == endCross[4]) and (startCross[2] != -1)):
-			roadLine.append( startCross[2] )
-		elif ( (startCross[3] == endCross[1] or startCross[3] == endCross[2] or startCross[3] == endCross[3] or startCross[3] == endCross[4]) and (startCross[3] != -1)):
-			roadLine.append( startCross[3] )
-		elif ( (startCross[4] == endCross[1] or startCross[4] == endCross[2] or startCross[4] == endCross[3] or startCross[4] == endCross[4]) and (startCross[4] != -1)):
-			roadLine.append( startCross[4] )
-		# else:
-		# print("warning",startCross,endCross)
-
-
-	# print ("roadLine=",roadLine)
-	return roadLine
-
-
-
-
-
 def createAnswer():
 
 	# [carID,startPos,endPos,maxSpeed,takeoffTime]
@@ -80,9 +49,8 @@ def createAnswer():
 		
 		oneCar = [data[0],data[4]]
 
-		crossLine = CNW.createNetwork(fS.carData,fS.roadData,fS.crossData,data[1],data[2])
-		roadLine = cross2road(crossLine)
-
+		roadLine  = CNW.createNetwork(fS.carData,fS.roadData,fS.crossData,data[1],data[2])
+		
 		oneCar.extend(roadLine)
 		
 		# print ( "oneCar=",oneCar )
