4cfd76b15d5c6f337511f187786b8461682ae7f7
Ruige_Lee <295054118@qq.com>
2019-03-23 16:53:27 +0800

    带上上一级的方向，直线优先，左转优先

--- SDK/CodeCraft-2019/src/ol_crossNetWorkOnline.py
+++ SDK/CodeCraft-2019/src/ol_crossNetWorkOnline.py
@@ -4,7 +4,7 @@
 # @Author: Ruige_Lee
 # @Date:   2019-03-19 11:00:06
 # @Last Modified by:   Ruige_Lee
-# @Last Modified time: 2019-03-23 10:21:28
+# @Last Modified time: 2019-03-23 16:52:15
 # @Email: 295054118@whut.edu.cn"
 
 
@@ -37,13 +37,13 @@ class crossNetwork():
 		pass
 	
 
-	def fw_checkroadVV(self,startCross,roadId):
-
+	def fw_checkroadVV(self,startCross,roadId,preDir):
+		retDir = 0
 		nextCross = -1
 		# [roadID,roadLength,maxSpeed,chnNum,startID,endID,doubleBool]
 		
 		if ( roadId == -1 ):
-			return nextCross,0
+			return nextCross,0,retDir
 
 		# 默认路就是5000开始的
 		oneRoad = self.roadData[roadId-5000] 
@@ -67,17 +67,39 @@ class crossNetwork():
 	#如果这条路是有效的，在这里调用子函数计算这条路的价值，然后一起返回,
 	#可以参考的参数：道路的最高速，当前车的最高速，道路长度，车道数
 	
-				roadValue = self.wCard.queryCardOnce(roadId,roadDir)
+				# roadValue = self.wCard.queryCardOnce(roadId,roadDir)
 				# roadValue = self.RDV.RD_Value(oneRoad[1],oneRoad[2],oneRoad[3])
-
+				
+				# [crossID,roadID1,roadID2,roadID3,roadID4]
+				if ( roadId == self.crossData[startCross-1][1]  ):
+					retDir = 1
+				elif ( roadId == self.crossData[startCross-1][2]  ):
+					retDir = 2
+				elif ( roadId == self.crossData[startCross-1][3]  ):
+					retDir = 3
+				elif ( roadId == self.crossData[startCross-1][4]  ):
+					retDir = 4
+				else:
+					print ("error!")
+					while(1):
+						pass
+				# 直行
+				if ( retDir == preDir ):
+					roadValue = 4
+				elif ( (preDir == 1 and retDir == 4) or (preDir == 2 and retDir == 1) or (preDir == 3 and retDir == 2) or (preDir == 4 and retDir == 3)):
+					roadValue = 3
+				elif ( (preDir == 1 and retDir == 4) or (preDir == 2 and retDir == 1) or (preDir == 3 and retDir == 2) or (preDir == 4 and retDir == 3)):
+					roadValue = 2
+				else:
+					roadValue = 0
 #####################################################################
 
 
-				return nextCross,roadValue
+				return nextCross,roadValue,retDir
 
 
 		# print ( "checkroadVaild=Return" ,-1)
-		return -1,0
+		return -1,0,retDir
 
 
 
@@ -85,7 +107,7 @@ class crossNetwork():
 	def fw_createcrossTree(self):
 
 		# 载入起始点，使用格式，每层两级，一级表示层，一级对应上层的实体枝干
-		self.crossTree.append([[self.startCross]])
+		self.crossTree.append([[self.startCross,0]])
 
 		# 集合中删除起始点
 		del self.crossCollection[self.startCross-1]
@@ -105,14 +127,14 @@ class crossNetwork():
 
 
 					# 默认cross索引是从1开始的
-					crossInfo = self.crossData[crossId-1]
+					crossInfo = self.crossData[crossId[0]-1]
 					# print ("crossInfo=",crossInfo)
 
 					# 检查路标
-					cross1Temp,value1 = self.fw_checkroadVV(crossInfo[0],crossInfo[1])
-					cross2Temp,value2 = self.fw_checkroadVV(crossInfo[0],crossInfo[2])
-					cross3Temp,value3 = self.fw_checkroadVV(crossInfo[0],crossInfo[3])
-					cross4Temp,value4 = self.fw_checkroadVV(crossInfo[0],crossInfo[4])
+					cross1Temp,value1,preDir1 = self.fw_checkroadVV(crossInfo[0],crossInfo[1],crossId[1])
+					cross2Temp,value2,preDir2 = self.fw_checkroadVV(crossInfo[0],crossInfo[2],crossId[1])
+					cross3Temp,value3,preDir3 = self.fw_checkroadVV(crossInfo[0],crossInfo[3],crossId[1])
+					cross4Temp,value4,preDir4 = self.fw_checkroadVV(crossInfo[0],crossInfo[4],crossId[1])
 
 					# 本级为叶子，则下一级留空以防止错位
 					leavesCheck = []
@@ -121,32 +143,32 @@ class crossNetwork():
 
 ## 价值函数排序可以在这里做，本级cross已经确定，下级cross的价值可以从前面return 回来，下级cross放在前面会先被for到
 #
-					SortListTemp = [[cross1Temp,value1],[cross2Temp,value2],[cross3Temp,value3],[cross4Temp,value4]]
+					SortListTemp = [[cross1Temp,value1,preDir1],[cross2Temp,value2,preDir2],[cross3Temp,value3,preDir3],[cross4Temp,value4,preDir4]]
 					# print( "SortListTemp=B",SortListTemp )
 					SortListTemp.sort(key=lambda x:x[1],reverse=True)
 					# print( "SortListTemp=A",SortListTemp )
 
 
-					cross1 = SortListTemp[0][0]
-					cross2 = SortListTemp[1][0]
-					cross3 = SortListTemp[2][0]
-					cross4 = SortListTemp[3][0]
+					cross1 = [ SortListTemp[0][0], SortListTemp[0][2]]
+					cross2 = [ SortListTemp[1][0], SortListTemp[1][2]]
+					cross3 = [ SortListTemp[2][0], SortListTemp[2][2]]
+					cross4 = [ SortListTemp[3][0], SortListTemp[3][2]]
 
 ##################################################
 					
 
 
 
-					if ( cross1 != -1 ):
+					if ( cross1[0] != -1 ):
 						leavesCheck.append(cross1)
-					if ( cross2 != -1 ):	
+					if ( cross2[0] != -1 ):	
 						leavesCheck.append(cross2)
-					if ( cross3 != -1 ):	
+					if ( cross3[0] != -1 ):	
 						leavesCheck.append(cross3)
-					if ( cross4 != -1 ):	
+					if ( cross4[0] != -1 ):	
 						leavesCheck.append(cross4)
 					
-					if ( (cross1 == self.endCross) or (cross2 == self.endCross) or (cross3 == self.endCross) or (cross4 == self.endCross) ):
+					if ( (cross1[0] == self.endCross) or (cross2[0] == self.endCross) or (cross3[0] == self.endCross) or (cross4[0] == self.endCross) ):
 						flagFound = 1
 
 					# 本路口级增加一个枝干组
