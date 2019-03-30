34df2b0385b3322881794e4076649c44db67436d
whutddkUbuntu16 <295054118@whut.edu.cn>
2019-03-30 10:36:21 +0800

    短路优先 //20 3258 3239

--- SDK/CodeCraft-2019/src/ol_roadValueRisk.py
+++ SDK/CodeCraft-2019/src/ol_roadValueRisk.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # @File Name: ol_roadValueRisk.py
-# @File Path: M:\MAS2\dark_PRJ\HWCC2019\SDK\CodeCraft-2019\src\ol_roadValueRisk.py
+# @File Path: /home/whutddk/下载/HWCC2019/SDK/CodeCraft-2019/src/ol_roadValueRisk.py
 # @Author: Ruige_Lee
 # @Date:   2019-03-21 16:15:16
-# @Last Modified by:   Ruige_Lee
-# @Last Modified time: 2019-03-25 11:18:25
+# @Last Modified by:   whutddkUbuntu16
+# @Last Modified time: 2019-03-30 10:30:17
 # @Email: 295054118@whut.edu.cn"
 
 
@@ -27,7 +27,7 @@ class roadValue():
 		# 默认长度是速度2~5倍
 		self.value = 0
 
-		self.value = rdSpeed*(0) - (rdLength*0) + (rdChnNum*1)
+		self.value = rdSpeed*(0) - (rdLength*1) + (rdChnNum*0)
 
 		return self.value
 
