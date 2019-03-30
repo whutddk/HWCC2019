3e672a5329ff7184446c2d907477f180080b8c19
whutddkUbuntu16 <295054118@whut.edu.cn>
2019-03-30 15:01:10 +0800

    regress pass

--- SDK/CodeCraft-2019/src/ol_crossNetWorkOnline.py
+++ SDK/CodeCraft-2019/src/ol_crossNetWorkOnline.py
@@ -4,7 +4,7 @@
 # @Author: Ruige_Lee
 # @Date:   2019-03-25 08:50:11
 # @Last Modified by:   whutddkUbuntu16
-# @Last Modified time: 2019-03-30 14:28:55
+# @Last Modified time: 2019-03-30 14:57:37
 # @Email: 295054118@whut.edu.cn"
 
 # @File Name: ol_crossNetWorkOnline.py
@@ -428,7 +428,7 @@ class crossNetwork():
 		additionalTime = 0
 
 		for ans in range(0,len(speed6)):
-			schTime = additionalTime//34 + offset
+			schTime = additionalTime//33 + offset
 			if ( speed6[ans][2] < schTime  ):
 				speed6[ans][2] = schTime	
 			additionalTime = additionalTime + 1 
