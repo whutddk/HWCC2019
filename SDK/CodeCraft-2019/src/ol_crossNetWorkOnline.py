71efb9cda46ec2991d94beb391ea49977bad0808
whutddkUbuntu16 <295054118@whut.edu.cn>
2019-03-30 12:18:58 +0800

    2199 2329

--- SDK/CodeCraft-2019/src/ol_crossNetWorkOnline.py
+++ SDK/CodeCraft-2019/src/ol_crossNetWorkOnline.py
@@ -4,7 +4,7 @@
 # @Author: Ruige_Lee
 # @Date:   2019-03-25 08:50:11
 # @Last Modified by:   whutddkUbuntu16
-# @Last Modified time: 2019-03-30 12:08:41
+# @Last Modified time: 2019-03-30 12:13:58
 # @Email: 295054118@whut.edu.cn"
 
 # @File Name: ol_crossNetWorkOnline.py
@@ -393,7 +393,7 @@ class crossNetwork():
 
 		additionalTime = 0		
 		for ans in range(0,len(speed12)):
-			schTime = additionalTime//35 + offset
+			schTime = additionalTime//40 + offset
 			if ( speed12[ans][2] < schTime ):
 				speed12[ans][2] = schTime
 			else:
